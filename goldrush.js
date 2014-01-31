1efe35da75760fe39d796d12439ddceb30d08652
Karin <karin.j.alvarez@gmail.com>
2014-01-31 15:11:36 -0700

    Next commit

--- goldrush.js
+++ goldrush.js
@@ -10,12 +10,27 @@ $(document).ready(function(){
 
 		var locationNotes = prompt("Would you like to enter notes about this location?");
 			console.log(locationNotes);
-			var locationNotesdiv = $('<div class="noteStyle">locationNotes</div>');
+			var locationNotesdiv = $('<div class="noteStyle">' + locationNotes + '</div>');
+			locationNotesdiv.css({
+				'top': e.pageY,
+				'left': e.pageX,
+				'background-color' : '#CFB53B',
+				'font-family' : 'Ceviche One',
+				'border-radius' : '5px',
+				'font-size' : '36px'
+			});
 
 
 		goldSource.on('mouseenter', function() {
+			e.preventDefault();
 			console.log(locationNotes);
 			$('.container').append(locationNotesdiv);
+			locationNotesdiv.show();
+
+		goldSource.on('mouseleave', function(){
+			e.preventDefault();
+			locationNotesdiv.hide();
+		})
 
 	});
 
